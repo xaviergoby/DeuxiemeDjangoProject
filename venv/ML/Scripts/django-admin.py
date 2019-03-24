124f35f9bff643b0cb5aa49ec4c3aa2d99db8016
Xavier Goby <xgoby@hotmail.com>
2019-03-25 00:22:09 +0100

    initial project version

---
+++ venv/ML/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\XGOBY\ThirdDjangoMainProjectDirFolder\venv\ML\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
